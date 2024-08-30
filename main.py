c704f7cd06fbcfbe499d619210accef00026b62a
rumbless <luodfss@gmail.com>
2024-08-30 14:21:28 +0800

    更新关注人员和推送

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ logging.basicConfig(
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S',
     filename='call.log',
-    encoding='utf-8'
+#    encoding='utf-8'
 )
 
 stock_url = "https://stock.xueqiu.com/v5/stock/portfolio/stock/list.json"
@@ -22,6 +22,7 @@ user_dict = {
     "薛定谔的牛": "3639429204",
     "复旦橙子橙": "3697768583",
     "Moss_LD": "3316855169",
+    "robo": "5712584562",
     # "猫猫": "9696783696"
 }
 
@@ -29,19 +30,18 @@ cube_url = "https://xueqiu.com/cubes/rebalancing/history.json"
 cube_data_previous = {}  # 第一次访问组合API
 cube_param = {"count": 20, "page": 1}
 cube_dict = {
-    # "薛定谔的牛": "ZH3221865",
+    "薛定谔的牛": "ZH3330084",
     # "行中衡": "ZH847468",
     "轮回666": "ZH3292517",
-    "花盆君": "ZH3125504",
+    "花盆君": "ZH3377835",
     "暮烟风雨": "ZH1739131",
-    "徐富贵冲冲冲": "ZH3165213",
-    "麻省锂公": "ZH3295255"
+    # "发哥": "ZH3259218",
 }
 
 session = requests.Session()
 session.headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-    'Cookie': "device_id=1cff0e058c5b49745650715ce680107c; bid=b300e536504d14d4617e5581f6923929_ldmhd58f; s=dw1px0n3bj; __utmz=1.1675305045.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); Hm_lvt_fe218c11eab60b6ab1b6f84fb38bcc4a=1683339616; u=9696783696; xq_is_login=1; cookiesu=731692581296810; xq_a_token=c83155692beaa7d4883a765636d1820636e2e19a; xq_id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOjk2OTY3ODM2OTYsImlzcyI6InVjIiwiZXhwIjoxNzAyNTIzNTg2LCJjdG0iOjE2OTk5MzE1ODYyMzIsImNpZCI6ImQ5ZDBuNEFadXAifQ.NNbk4ztEn-D7Xc_GsqwKfAb3VpiIybisFLxAg1A_0TUlifb21yIVYqXhH4Dl6iMnhDIy8MxudSXrCfiEExIiypftcrEbTgQqBVFNTAmT0o0B_SoQ0NPgCVeDUBEMWt9jqjO5di2GeW8idJJuoa9bgCaFqzPxCUDjGzWMEnWaQTSLGU5ejJ8VEZ64nG1y8M8NnYS6FJz5o91x4sf2S5Y9BNZqhNo-GgMzRgCVMPWlbs5DorE3dBfxlhy05rQKPJVE2YVA6Y8-xS59pJ2NdT8Boa5r6-qP6z_YiESyop8yWs8N2r4aOJ_MEj3Qe1d5LgBMmle1qrQ8M4fujMhmZzyjOQ; xq_r_token=dd62bfbb74e0b8a343893a1d3c0d0f411fe5e6f6; xqat=c83155692beaa7d4883a765636d1820636e2e19a; Hm_lvt_1db88642e346389874251b5a1eded6e3=1698824552,1698919905,1699839073,1699955455; snbim_minify=true; __utmc=1; acw_tc=2760779917002959251645231e7cd1682e0f39fb3c9868ae98167321a03569; is_overseas=0; __utma=1.630488574.1675305045.1700291913.1700295931.188; __utmt=1; __utmb=1.2.10.1700295931; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1700295934",
+    'Cookie': "xq_a_token=129e388aa28d79525f7d34343965facf5541bca6",
     'cache-control': "no-cache",
 }
 
@@ -99,7 +99,7 @@ while True:
                             push_deer_url3 = f"https://api2.pushdeer.com/message/push?pushkey=PDU25746TNdVSyL1JMuocwUTR9OQGiTyzDWrv2k0B&text={msg}"
                             requests.get(push_deer_url)
                             requests.get(push_deer_url2)
-                            requests.get(push_deer_url3)
+                            #requests.get(push_deer_url3)
 
                 if removed_symbols:
                     for stock in stock_data_previous[uid]:
@@ -126,21 +126,21 @@ while True:
             # 第一次访问 API，保存数据到变量中
             if cube_id not in cube_data_previous:
                 cube_data_previous[cube_id] = data_current['id']
-                logging.info(f"【{name}]组合数据初始化成功")
+                logging.info(f"【{name}】组合数据初始化成功")
             # 遍历检查数据，打印组合新调仓信息
             else:
                 for cube in cube_history:
                     if cube['id'] != cube_data_previous[cube_id]:
                         for one in cube['rebalancing_histories']:
-                            msg = f"【{name}】组合调仓信息: {one['stock_symbol']}:{one['stock_name']}:{one['price']}: {one['prev_weight_adjusted'] if one['prev_weight_adjusted'] else 0}%->{one['target_weight']}%"
+                            msg = f"【{name}】组合调仓信息: {one['stock_symbol']}:{one['stock_name']}:{one['price']}【{one['prev_weight_adjusted'] if one['prev_weight_adjusted'] else 0}%->{one['target_weight']}%】"
                             logging.info(msg)
                             push_deer_url = f"https://api2.pushdeer.com/message/push?pushkey=PDU23078Tvl3ShDVG3aYDrCO2Eqf9azouteT6F13q&text={msg}"
                             push_deer_url2 = f"https://api2.pushdeer.com/message/push?pushkey=PDU26203TfKUwbR46v1cDQpcHcVh9Ahw5heaMcgkR&text={msg}"
                             push_deer_url3 = f"https://api2.pushdeer.com/message/push?pushkey=PDU25746TNdVSyL1JMuocwUTR9OQGiTyzDWrv2k0B&text={msg}"
                             requests.get(push_deer_url)
-                            if cube_id != "ZH3125504" and cube_id != "ZH3295255":
+                            #requests.get(push_deer_url3)
+                            if cube_id != "ZH3377835" and cube_id != "ZH3318860" and cube_id != "ZH1739131": 
                                 requests.get(push_deer_url2)
-                                requests.get(push_deer_url3)
 
                     else:
                         cube_data_previous[cube_id] = data_current['id']
@@ -171,4 +171,4 @@ while True:
     #     logging.error(response.content.decode())
 
     # 等待一段时间后再次访问 API
-    time.sleep(30)
+    time.sleep(15)
