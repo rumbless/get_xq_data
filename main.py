88b10a9e3ef6cd6e6e4c67c2bb93ae24173ddc23
rumbless <luodfss@gmail.com>
2024-10-14 10:11:43 +0800

    优化无数据返回情况

--- main.py
+++ main.py
@@ -90,67 +90,69 @@ if __name__ == "__main__":
         for name, uid in user_dict.items():
             data_current = get_stock_data(uid)
 
-            # 第一次访问 API，保存数据到变量中
-            if uid not in stock_data_previous:
-                stock_data_previous[uid] = data_current
-                logging.info(f"【{name}】股票数据初始化成功")
+            if data_current:
+                # 第一次访问 API，保存数据到变量中
+                if uid not in stock_data_previous:
+                    stock_data_previous[uid] = data_current
+                    logging.info(f"【{name}】股票数据初始化成功")
 
-            # 非第一次访问 API，比较数据，找出新增和删除的股票信息
-            else:
-                symbols_previous = set(stock['symbol'] for stock in stock_data_previous[uid])
-                symbols_current = set(stock['symbol'] for stock in data_current)
-                new_symbols = symbols_current - symbols_previous
-                removed_symbols = symbols_previous - symbols_current
+                # 非第一次访问 API，比较数据，找出新增和删除的股票信息
+                else:
+                    symbols_previous = set(stock['symbol'] for stock in stock_data_previous[uid])
+                    symbols_current = set(stock['symbol'] for stock in data_current)
+                    new_symbols = symbols_current - symbols_previous
+                    removed_symbols = symbols_previous - symbols_current
 
-                if new_symbols:
-                    for stock in data_current:
-                        if stock['symbol'] in new_symbols:
-                            msg = f"【{name}】新增股票信息: " + stock['symbol'] + ":" + stock['name']
-                            logging.info(msg)
-                            for k, v in push_key_dict.items():
-                                if k != "ating":
-                                    send_push_deer_notification(v, msg)
-                                else:
-                                    if uid == '8282709675':
+                    if new_symbols:
+                        for stock in data_current:
+                            if stock['symbol'] in new_symbols:
+                                msg = f"【{name}】新增股票信息: " + stock['symbol'] + ":" + stock['name']
+                                logging.info(msg)
+                                for k, v in push_key_dict.items():
+                                    if k != "ating":
                                         send_push_deer_notification(v, msg)
+                                    else:
+                                        if uid == '8282709675':
+                                            send_push_deer_notification(v, msg)
 
-                if removed_symbols:
-                    for stock in stock_data_previous[uid]:
-                        if stock['symbol'] in removed_symbols:
-                            msg = f"【{name}】删除股票信息: " + stock['symbol'] + ":" + stock['name']
-                            logging.info(msg)
-                            send_push_deer_notification(push_key_dict['liujunyu'], msg)
-                            send_push_deer_notification(push_key_dict['me'], msg)
-                            if uid == '8282709675':
-                                send_push_deer_notification(push_key_dict['ating'], msg)
+                    if removed_symbols:
+                        for stock in stock_data_previous[uid]:
+                            if stock['symbol'] in removed_symbols:
+                                msg = f"【{name}】删除股票信息: " + stock['symbol'] + ":" + stock['name']
+                                logging.info(msg)
+                                send_push_deer_notification(push_key_dict['liujunyu'], msg)
+                                send_push_deer_notification(push_key_dict['me'], msg)
+                                if uid == '8282709675':
+                                    send_push_deer_notification(push_key_dict['ating'], msg)
 
-                # 更新保存的数据为当前数据
-                stock_data_previous[uid] = data_current
+                    # 更新保存的数据为当前数据
+                    stock_data_previous[uid] = data_current
 
         for name, cube_id in cube_dict.items():
             cube_param['cube_symbol'] = cube_id
 
             cube_history = get_cube_data(cube_id)
-            data_current = cube_history[0]
-            # 第一次访问 API，保存数据到变量中
-            if cube_id not in cube_data_previous:
-                cube_data_previous[cube_id] = data_current['id']
-                logging.info(f"【{name}】组合数据初始化成功")
-            # 遍历检查数据，打印组合新调仓信息
-            else:
-                for cube in cube_history:
-                    if cube['id'] != cube_data_previous[cube_id]:
-                        for one in cube['rebalancing_histories']:
-                            msg = f"【{name}】组合调仓信息: {one['stock_symbol']}:{one['stock_name']}:{one['price']}【{one['prev_weight_adjusted'] if one['prev_weight_adjusted'] else 0}%->{one['target_weight']}%】"
-                            logging.info(msg)
-                            send_push_deer_notification(push_key_dict['me'], msg)
-                            if cube_id == "ZH3292517":
-                                send_push_deer_notification(push_key_dict['liujunyu'], msg)
-                                send_push_deer_notification(push_key_dict['ating'], msg)
+            if cube_history:
+                data_current = cube_history[0]
+                # 第一次访问 API，保存数据到变量中
+                if cube_id not in cube_data_previous:
+                    cube_data_previous[cube_id] = data_current['id']
+                    logging.info(f"【{name}】组合数据初始化成功")
+                # 遍历检查数据，打印组合新调仓信息
+                else:
+                    for cube in cube_history:
+                        if cube['id'] != cube_data_previous[cube_id]:
+                            for one in cube['rebalancing_histories']:
+                                msg = f"【{name}】组合调仓信息: {one['stock_symbol']}:{one['stock_name']}:{one['price']}【{one['prev_weight_adjusted'] if one['prev_weight_adjusted'] else 0}%->{one['target_weight']}%】"
+                                logging.info(msg)
+                                send_push_deer_notification(push_key_dict['me'], msg)
+                                if cube_id == "ZH3292517":
+                                    send_push_deer_notification(push_key_dict['liujunyu'], msg)
+                                    send_push_deer_notification(push_key_dict['ating'], msg)
 
-                    else:
-                        cube_data_previous[cube_id] = data_current['id']
-                        break
+                        else:
+                            cube_data_previous[cube_id] = data_current['id']
+                            break
 
         # 等待一段时间后再次访问 API
         time.sleep(15)
