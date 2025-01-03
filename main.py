ca7ab44868280f40399f64089621e0fdb487b2fa
rumbless <luodfss@gmail.com>
2025-01-03 19:24:34 +0800

    增加公众号文章更新检测

--- main.py
+++ main.py
@@ -38,6 +38,28 @@ cube_dict = {
     "只胡清一色": "ZH3337506",
 }
 
+# 全局变量，存储文章标题
+global_titles = {}
+
+# 微信公众号信息
+gzh = {
+    "猫比刀": "Mzg2NzcxMjE1NA==",
+    # 可以添加更多的公众号
+}
+
+# 请求头
+Cookie = "appmsglist_action_3885240684=card; ptcz=af37a65e6383dd175ad7d2e3d4a875121205540c385fc1ba21eea797cd3b96d8; pgv_pvid=3868357759; iip=0; _qimei_q36=; _qimei_h38=fded8356a3fe8fbc1c50b5ff02000000018102; RK=GpUgQRyXXN; pac_uid=0_97f4890689ccc; qq_domain_video_guid_verify=7f9421e3f68bb889; eas_sid=k1q7q2J0K5E7L5E957y9U3J0k7; suid=user_0_97f4890689ccc; _uetvid=c428f2e07ae911ef812d9b383187bd74; wxuin=35887555214419; ua_id=ZPCBxBjPLIA0hSQPAAAAAC5kkNXluE1p-C5WII0MjYM=; _clck=rbn39m|1|fs9|0; uuid=49b80a743576e4a6dabbc5f83c387c16; slave_bizuin=3885240684; rand_info=CAESIJVC7zIweyKc6S1alMXWN1zNA0p5iRf0Vr3YquUSaUf5; xid=1ed81e170374f1021465256db13b62eb; mm_lang=zh_CN; slave_user=gh_ded3be315838; slave_sid=RUM0eVhtbzFlWFFPd2s0XzFUeWdVMGoxbVJYcVN3bExjcDduTFpqYVNGWV9xaTZkQ0JmNW1Rd2ZHTDZIZmhmaml3YVN6WmdsQWRsdVRDVmdieVNvZFlqR18wZk1SOHdOWlhrNGFkbGd2SHNGUW5iVEN3bm9VVmlqN1pNb3ozdzJYWjZlamlrRmZSU1NwdFBW; bizuin=3885240684; data_bizuin=3885240684; data_ticket=K5hfq3E3ki1KRFkV0U2pjjRiFw1KFsDbEeKwT1TcSzHiEwYGmsXcY0aKPa/834Ce; _clsk=1r7gsgx|1735889059997|3|1|mp.weixin.qq.com/weheat-agent/payload/record; rewardsn=; wxtokenkey=777"
+headers = {
+    "Cookie": Cookie,
+    "User-Agent": "Mozilla/5.0 (Linux; Android 10; YAL-AL00 Build/HUAWEIYAL-AL00) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.64 HuaweiBrowser/10.0.1.335 Mobile Safari/537.36"
+}
+
+# 微信公众号的token
+token = "1316911862"
+
+# 定时访问间隔（例如，每15秒访问一次）
+INTERVAL = 15  # 15 s
+
 push_key_dict = {
     "me": "PDU23078Tvl3ShDVG3aYDrCO2Eqf9azouteT6F13q",
     "liujunyu": "PDU26203TfKUwbR46v1cDQpcHcVh9Ahw5heaMcgkR",
@@ -84,6 +106,43 @@ def send_push_deer_notification(push_key, message):
         requests.get(url)
     except RequestException as e:
         logging.error(f"发送推送通知失败：{e}")
+
+
+def get_article_titles(fake_id):
+    url = "https://mp.weixin.qq.com/cgi-bin/appmsg"
+    params = {
+        "token": token,
+        "lang": "zh_CN",
+        "f": "json",
+        "ajax": "1",
+        "action": "list_ex",
+        "begin": 0,
+        "count": "2",
+        "query": "",
+        "fakeid": fake_id,
+        "type": "9",
+    }
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        return response.json().get("app_msg_list", [])
+    return []
+
+
+def check_updates():
+    for gzh_name, fake_id in gzh.items():
+        article_list = get_article_titles(fake_id)
+        new_titles = [article['title'] for article in article_list]
+        if fake_id not in global_titles:
+            # 第一次访问，不发送通知
+            global_titles[fake_id] = new_titles
+            logging.info(f"Initialized titles for {gzh_name}.")
+        elif global_titles[fake_id] != new_titles:
+            # 标题不相同，发送通知
+            gzh_msg = f"【{gzh_name}】有文章更新【{new_titles}】!"
+            global_titles[fake_id] = new_titles
+            for _, push_value in push_key_dict.items():
+                send_push_deer_notification(push_value, gzh_msg)
+            logging.info(gzh_msg)
 
 
 if __name__ == "__main__":
@@ -155,6 +214,7 @@ if __name__ == "__main__":
                             cube_data_previous[cube_id] = data_current['id']
                             break
 
+        # 检查公众号是否更新
+        check_updates()
         # 等待一段时间后再次访问 API
-        time.sleep(15)
-
+        time.sleep(INTERVAL)
