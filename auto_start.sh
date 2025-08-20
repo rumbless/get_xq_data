51c70af3384f55c161e78f63f7430210a140224a
rumbless <luodfss@gmail.com>
2025-08-20 10:42:11 +0800

    add cron

--- auto_start.sh
+++ auto_start.sh
@@ -1,3 +1,4 @@
+# * * * * * sh /data/get_xq_data/auto_start.sh
 if ! pgrep -f main.py >/dev/null; then
     echo "`date` - server process is not exist, auto restart!" >> /data/get_xq_data/call.log
     # 在这里添加重启服务器的命令
